133a595068273d3d16568d2a37694ecae12b87f3
Sachin Tekchandani <sachin6291@gmail.com>
2024-03-23 00:41:37 +0000

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+
+
+function App() {
+
+
+  return (
+    <>
+      <header className=" bg-teal-400 py-5">
+        <h1 className=" text-center text-4xl font-bold">Calculator of Cunsumption and tips</h1>
+      </header>
+
+  
+    </>
+  )
+}
+
+export default App
